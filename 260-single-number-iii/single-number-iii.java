9e7a4294196a8a52d46d730ddf38c0d8002c7a02
thilagaraaj89654 <thilagaraaj896@gmail.com>
2026-09-16 09:19:46 +0530

    Time: 1 ms (100.00%) | Memory: 47.8 MB (76.86%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        int diff = xor & -xor;
+
+        int[] result = new int[2];
+        for (int num : nums) {
+            if ((num & diff) == 0) {
+                result[0] ^= num;
+            } else {
+                result[1] ^= num;
+            }
+        }
+
+        return result;
+    }
+}
