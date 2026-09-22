00e4b45a651c1b7cee7937e1a9de3ae5850c7d19
thilagaraaj89654 <thilagaraaj896@gmail.com>
2026-09-22 09:55:07 +0530

    Time: 237 ms (14.18%) | Memory: 54.2 MB (29.04%) - LeetSync

--- 218-the-skyline-problem/the-skyline-problem.java
+++ 218-the-skyline-problem/the-skyline-problem.java
@@ -1,13 +1,10 @@
 class Solution {
     public List<List<Integer>> getSkyline(int[][] buildings) {
-        List<int[]> events = new ArrayList<>();
-        
+        List<int[]> events = new ArrayList<>();        
         for (int[] b : buildings) {
             events.add(new int[]{b[0], -b[2]}); 
             events.add(new int[]{b[1], b[2]});  
         }
-
-        // Sort events: primary by x-coordinate, secondary by height
         Collections.sort(events, (a, b) -> {
             if (a[0] != b[0]) return Integer.compare(a[0], b[0]);
             return Integer.compare(a[1], b[1]);
