d8740a47cb4eed438becab24471fa2917f1fa2d4
thilagaraaj89654 <thilagaraaj896@gmail.com>
2026-09-24 11:32:58 +0530

    Time: 1 ms (85.56%) | Memory: 44.2 MB (86.64%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -4,19 +4,16 @@ class Solution {
         backtrack(n, 0, 0, new StringBuilder(), result);
         return result;
     }
-
     private void backtrack(int n, int openb, int closeb, StringBuilder current, List<String> result) {
         if (current.length() == 2 * n) {
             result.add(current.toString());
             return;
         }
-
         if (openb < n) {
             current.append('(');
             backtrack(n, openb + 1, closeb, current, result);
             current.deleteCharAt(current.length() - 1);
         }
-
         if (closeb < openb) {
             current.append(')');
             backtrack(n, openb, closeb + 1, current, result);
